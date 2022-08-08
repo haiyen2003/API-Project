951659a01f5edbf0a27efe4c09be91a51c07eb60
hai yen <nhyen.93@gmail.com>
2022-08-08 07:08:16 -0700

    bug

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -270,10 +270,10 @@ router.post('/:spotId/bookings', requireAuth, async (req, res, next) => {
 
     //create bookings
     const newBooking = await Booking.create({
-      //  spotId: spotId,
-        userId: user.id,
         startDate: startDate,
-        endDate: endDate
+        endDate: endDate,
+        spotId: spotId,
+          userId: user.id
     });
     return res.json(newBooking);
 })
