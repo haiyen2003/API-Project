f213c468b484e8086ba30918a5b19177965f5866
hai yen <nhyen.93@gmail.com>
2022-08-08 03:07:07 -0700

    delete review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -13,22 +13,22 @@ const { check } = require('express-validator');
 
 const validateReview = [
     check('review')
-    .exists({checkFalsy: true})
-    .withMessage('Review text is required'),
+        .exists({ checkFalsy: true })
+        .withMessage('Review text is required'),
     check('stars')
-    .exists({checkFalsy: true})
-    .isInt({min:1, max:5})
-    .withMessage('Stars must be an integer from 1 to 5'),
+        .exists({ checkFalsy: true })
+        .isInt({ min: 1, max: 5 })
+        .withMessage('Stars must be an integer from 1 to 5'),
     handleValidationErrors
 ];
 
 //get all the Review by current User
 router.get('/current', restoreUser, async (req, res, next) => {
-    const {user} = req;
-    if(user){
+    const { user } = req;
+    if (user) {
         const currReview = await Review.findAll({
-            where: {userId: user.id},
-            include: [{model: User}]
+            where: { userId: user.id },
+            include: [{ model: User }]
         });
         return res.json(currReview);
     }
@@ -37,12 +37,12 @@ router.get('/current', restoreUser, async (req, res, next) => {
 
 //create and return a new image for a review by ID
 
-router.post('/:reviewId/images', requireAuth, async(req, res, next) =>{
-    const {reviewId} = req.params;
-    const {url, previewImage} = req.body;
+router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
+    const { reviewId } = req.params;
+    const { url, previewImage } = req.body;
 
     const thisReview = await Review.findByPk(reviewId);
-    if(!thisReview){
+    if (!thisReview) {
         const err = new Error("Review couldn't be found");
         err.status = 404;
         return next(err);
@@ -53,7 +53,7 @@ router.post('/:reviewId/images', requireAuth, async(req, res, next) =>{
             spotId: thisReview.spotId
         }
     })
-    if(thisReviewImages.length >10){
+    if (thisReviewImages.length > 10) {
         const err = new Error('Maximum number of images for this resource was reached');
         err.status = 403;
         return next(err);
@@ -74,31 +74,55 @@ router.post('/:reviewId/images', requireAuth, async(req, res, next) =>{
 })
 
 //edit a review
-router.put('/:reviewId', requireAuth, validateReview, async (req, res, next) =>{
-    const {reviewId} = req.params;
-    const {review, stars} = req.body;
+router.put('/:reviewId', requireAuth, validateReview, async (req, res, next) => {
+    const { reviewId } = req.params;
+    const { review, stars } = req.body;
     const { user } = req;
 
     const thisReview = await Review.findByPk(reviewId);
-    if(!thisReview) {
+    if (!thisReview) {
         const err = new Error("Review couldn't be found");
         err.status = 404;
         return next(err);
     }
 
- //check current user
+    //check current user
     if (thisReview.userId !== user.id) {
-    const err = new Error("Cannot edit Review! Review must belong to current user.");
-    err.status = 403;
-    return next(err);
-}
-else {
-    thisReview.review = review;
-    thisReview.stars = stars;
-    await thisReview.save();
-    return res.json(thisReview);
-}
+        const err = new Error("Cannot edit Review! Review must belong to current user.");
+        err.status = 403;
+        return next(err);
+    }
+    else {
+        thisReview.review = review;
+        thisReview.stars = stars;
+        await thisReview.save();
+        return res.json(thisReview);
+    }
 })
 
+//delete a review
+
+router.delete('/:reviewId', requireAuth, async (req, res, next) => {
+    const { reviewId } = req.params;
+    const thisReview = await Review.findByPk(reviewId);
+    const { user } = req;
+    if (!thisReview) {
+        const err = new Error("Review couldn't be found");
+        err.status = 404;
+        return next(err);
+    }
+    if (thisReview.userId !== user.id) {
+        const err = new Error("Cannot delete Review! Review must belong to current user.");
+        err.status = 403;
+        return next(err);
+    }
+    else {
+        await thisReview.destroy();
+        return res.json({
+            "message": "Successfully deleted",
+            "statusCode": 200
+        });
+    };
+});
 
 module.exports = router;
